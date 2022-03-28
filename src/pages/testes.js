82d632c2e0b035ccc08477bfc9e3e7ed44fb2eda
Leonardo <leojn8@gmail.com>
2022-03-27 23:42:48 -0300

    atual

--- src/pages/testes.js
+++ src/pages/testes.js
@@ -1,13 +1,35 @@
-import { Flex } from "@chakra-ui/react";
-
+import { Button, Flex, Text } from "@chakra-ui/react";
+import data from "../../testeProd.json";
 
 const teste = () => {
-  const provas = ["prova1", "prova2", "prova3", "prova4", "prova5"];
+  const number = ["10", "10", "10", "25", "10"];
+  const numberData = data.produtos.map((n) => {
+    n.salário;
+  });
+  const tudo = { ...numberData };
+  const initialValue = "";
+  const sumWithInitial = number.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  var soma = numberData.reduce(function (soma, i) {
+    return soma + i;
+  });
+
+  console.log(tudo);
+
   return (
     <>
       <Flex>
         <Button type="button">aperte</Button>
+        <Text>{numberData}</Text>
+
+        <Text>{sumWithInitial}</Text>
       </Flex>
+      <Text>{soma}</Text>
     </>
   );
 };
+export default teste;
